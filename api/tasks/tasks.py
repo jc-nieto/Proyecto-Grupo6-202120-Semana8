75c5cd1347ba2eb0aa637322738315f34114649c
Daniel Alberto Larios Torres <38303618+dalt45@users.noreply.github.com>
2021-10-23 23:55:44 +0200

    Added email send

--- api/tasks/tasks.py
+++ api/tasks/tasks.py
@@ -1,9 +1,14 @@
 from celery import Celery
 import os
+import smtplib, ssl
+import db
 
-from models import Tarea
+port = 587  # For SSL
+password = input("Type your password and press enter: ")
 
-from config import CELERY_RESULT_BACKEND,CELERY_BROKER_URL
+from models import Tarea, Usuario
+
+from config import CELERY_RESULT_BACKEND, CELERY_BROKER_URL
 
 celery_app = Celery(
     'tareas',
@@ -11,26 +16,40 @@ celery_app = Celery(
     broker=CELERY_BROKER_URL
 )
 
+
 def changeTaskState(task_id):
-    task:Tarea = Tarea.get_by_id(task_id)
+    task: Tarea = Tarea.get_by_id(task_id)
     task.estado = 'processed'
     task.save()
 
 
 def convertFile(task_id):
-    task:Tarea = Tarea.get_by_id(task_id)
-    os.system('ffmpeg -i {} {}'.format(task.inputpath,task.outputpath))
+    task: Tarea = Tarea.get_by_id(task_id)
+    usuario: Usuario = Usuario.get_by_id(task.usuario_task)
+    os.system('ffmpeg -i {} {}'.format(task.inputpath, task.outputpath))
+    context = ssl.create_default_context()
+    with smtplib.SMTP("smtp.sendgrid.net", port) as server:
+        server.ehlo()
+        server.starttls()
+        server.ehlo()
+        server.login("apikey", password='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')
+        subject = 'Prueba'
+        body = 'Su archivo ha sido procesado'
+        msg = f'From:daniel@crecyservices.io\nSubject:{subject}\n\n{body}'
+        server.sendmail("daniel@crecyservices.io", usuario.email, msg)
+
 
 def deleteTask(task_id):
-    task:Tarea = Tarea.get_by_id(task_id)
+    task: Tarea = Tarea.get_by_id(task_id)
     task.delete()
+
 
 @celery_app.task(name="procesar_tarea")
 def cron(id_task):
     convertFile(id_task)
     changeTaskState(id_task)
 
+
 @celery_app.task(name="borrar_tarea")
 def delete(id_task):
     deleteTask(id_task)
-    
