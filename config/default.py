3179b98b6fe7ec4030b9df41ad67966a95af2f30
Daniel Alberto Larios Torres <38303618+dalt45@users.noreply.github.com>
2021-10-30 13:43:46 +0200

    Changed Settings Redis Server

--- config/default.py
+++ config/default.py
@@ -5,5 +5,5 @@ SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:12345@localhost:5432/convertdb'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SHOW_SQLALCHEMY_LOG_MESSAGES = False
 JWT_SECRET_KEY = 'frase-secreta'
-CELERY_BROKER_URL='redis://localhost:6379/0',
-CELERY_RESULT_BACKEND='redis://localhost:6379/0'
+CELERY_BROKER_URL='redis://ec2-3-236-182-111.compute-1.amazonaws.com:6379/0',
+CELERY_RESULT_BACKEND='redis://ec2-3-236-182-111.compute-1.amazonaws.com:6379/0'
