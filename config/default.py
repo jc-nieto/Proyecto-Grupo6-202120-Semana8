05cdef95100b029382fc9842a862b46c61e86023
Daniel Alberto Larios Torres <38303618+dalt45@users.noreply.github.com>
2021-10-31 19:42:20 +0100

    Update default.py

--- config/default.py
+++ config/default.py
@@ -5,5 +5,5 @@ SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:gerzonEsUnCrack@mi-database-01.
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SHOW_SQLALCHEMY_LOG_MESSAGES = False
 JWT_SECRET_KEY = 'frase-secreta'
-CELERY_BROKER_URL='redis://44.199.204.13:6379/0',
+CELERY_BROKER_URL='redis://172.16.1.110:6379/0',
 CELERY_RESULT_BACKEND='redis://localhost:6379/0'
