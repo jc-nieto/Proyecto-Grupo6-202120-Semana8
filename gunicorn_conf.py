7bfd1d38732a75a087bbc29893d6cc37b439a920
Ubuntu <ubuntu@ip-172-16-1-123.ec2.internal>
2021-11-02 18:56:04 +0000

    conf

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,6 +1,5 @@
-bind = '0.0.0.0:5000 wsgi:app'
 worker_class = 'sync'
 loglevel = 'debug'
-accesslog = '/var/log/gunicorn/access_log_yourapp'
+accesslog = '/home/ubuntu/gunicorn/access_log_yourapp'
 acceslogformat = "%(h)s %(l)s %(u)s %(t)s %(r)s %(s)s %(b)s %(f)s %(a)s"
-errorlog = '/var/log/gunicorn/error_log_yourapp'
+errorlog = '/home/ubuntu/gunicorn/error_log_yourapp'
