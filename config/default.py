b660daef5eb6161aaa73e8c7b98cd0bf9550af81
EdgarLunaG-442 <ef.luna442@uniandes.edu.co>
2021-11-16 18:25:05 -0500

    se modifica celery broker

--- config/default.py
+++ config/default.py
@@ -5,5 +5,5 @@ SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:gerzonEsUnCrack@mydb.cg99sgyfzy
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SHOW_SQLALCHEMY_LOG_MESSAGES = False
 JWT_SECRET_KEY = 'frase-secreta'
-CELERY_BROKER_URL='redis://172.16.1.165:6379/0',
+CELERY_BROKER_URL='redis://172.16.1.23:6379/0',
 CELERY_RESULT_BACKEND='redis://localhost:6379/0'
