40d4783a81d4f1b4e612da2366e7786fbcf1adfc
EdgarLuna <ef.luna442@uniandes.edu.co>
2021-11-21 18:52:58 -0500

    Se modifican coonfigs

--- config/default.py
+++ config/default.py
@@ -1,9 +1,9 @@
 PROPAGATE_EXCEPTIONS = True
 
 # Database configuration
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:gerzonEsUnCrack@mydb.cg99sgyfzyzj.us-east-1.rds.amazonaws.com:3306/myDB'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:gerzonEsUnCrack@mi-database-01.cw2jffeaugvm.us-east-1.rds.amazonaws.com:3306/myDB'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SHOW_SQLALCHEMY_LOG_MESSAGES = False
 JWT_SECRET_KEY = 'frase-secreta'
-CELERY_BROKER_URL='redis://172.16.1.101:6379/0',
+CELERY_BROKER_URL='redis://172.16.1.110:6379/0',
 CELERY_RESULT_BACKEND='redis://localhost:6379/0'
