b09456f0bec548261a9d15798fbf74fe15c69ac2
EdgarLuna <ef.luna442@uniandes.edu.co>
2021-11-22 10:22:18 -0500

    nueva instancia para semana 4

--- models/tarea_model.py
+++ models/tarea_model.py
@@ -1,5 +1,5 @@
 from db import BaseModelMixin, db
-
+from sqlalchemy.sql import func
 
 class Tarea(db.Model, BaseModelMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -8,5 +8,7 @@ class Tarea(db.Model, BaseModelMixin):
     outputpath = db.Column(db.String(128), nullable=True)
     inputformat = db.Column(db.String(128))
     outputformat = db.Column(db.String(128))
+    time_created = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    time_updated = db.Column(db.DateTime(timezone=True), onupdate=func.now())
     estado = db.Column(db.String(128), default='uploaded', nullable=True)
     usuario_task = db.Column(db.Integer, db.ForeignKey("usuario.id"))
