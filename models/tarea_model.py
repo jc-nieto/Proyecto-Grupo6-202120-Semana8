dbb55ebb59a88b7576189f1cb2952e4a63c2cf2c
EdgarLuna <ef.luna442@uniandes.edu.co>
2021-11-21 20:43:29 -0500

    SE modifica modelo para guardar registros

--- models/tarea_model.py
+++ models/tarea_model.py
@@ -1,4 +1,5 @@
 from db import BaseModelMixin, db
+from sqlalchemy.sql import func
 
 
 class Tarea(db.Model, BaseModelMixin):
@@ -7,6 +8,8 @@ class Tarea(db.Model, BaseModelMixin):
     inputpath = db.Column(db.String(128))
     outputpath = db.Column(db.String(128), nullable=True)
     inputformat = db.Column(db.String(128))
+    time_created = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    time_updated = db.Column(db.DateTime(timezone=True), onupdate=func.now())
     outputformat = db.Column(db.String(128))
     estado = db.Column(db.String(128), default='uploaded', nullable=True)
     usuario_task = db.Column(db.Integer, db.ForeignKey("usuario.id"))
