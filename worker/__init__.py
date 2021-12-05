bd3d983324d9dc18b634363517b3229efbf1e211
jc-nieto <jc.nieto@uniandes.edu.co>
2021-12-05 11:24:46 -0500

    Correccion comsumer

--- worker/__init__.py
+++ worker/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from db import db
 import logging
-import consumer
+from .consumer import process_messages
 
 def create_app(settings_module):
     app = Flask(__name__)
@@ -18,5 +18,5 @@ def create_app(settings_module):
 
     # Disable strict URL finishing mode with /
     app.url_map.strict_slashes = False
-    worker = consumer.process_messages()
+    worker = process_messages()
     return app
