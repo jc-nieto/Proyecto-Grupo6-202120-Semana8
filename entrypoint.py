afe9b43f44b9cc08855a81f4480ea63bc4e815aa
jc-nieto <jc.nieto@uniandes.edu.co>
2021-12-05 10:28:13 -0500

    Ajustes en entrypoint

--- entrypoint.py
+++ entrypoint.py
@@ -2,7 +2,7 @@ try:
     from worker import create_app #, consumer
 
     settings_module = 'config.default'
-    create_app(settings_module)
+    worker = create_app(settings_module)
     #worker = consumer.process_messages()
 
     
